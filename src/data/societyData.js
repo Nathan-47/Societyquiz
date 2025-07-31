c83a2245b1bf9b1357df8022f312710606f77bda
nathan-47 <nathan.parchment47@gmail.com>
2025-07-31 18:32:23 +0100

    liberation societies added

---
+++ src/data/societyData.js
@@ -0,0 +1,82 @@
+const societyData = {
+  "Liberation": [
+    {
+      name: "Disabled Students",
+      img: "https://www.bcusu.com/asset/Organisation/9508/DISABLED%20STUDENT%20(1).png",
+      link: "https://www.bcusu.com/organisation/disabledstudents/",
+      alt: "Disabled Students"
+    },
+    {
+      name: "International Society",
+      img: "https://www.bcusu.com/asset/Organisation/12252/International%20Soc(background)%20logo.png",
+      link: "#"
+    },
+    {
+      name: "LGBTQ+ Society",
+      img: "https://www.bcusu.com/asset/Organisation/6099/LGBT_Liger.png",
+      link: "#"
+    },
+    {
+      name: "Mature Students",
+      img: "https://www.bcusu.com/asset/Organisation/9223/Mature(1)(1)(1).png",
+      link: "#"
+    },
+    {
+      name: "Mental Health",
+      img: "https://www.bcusu.com/asset/Organisation/10568/Screenshot%202022-09-12%20at%2010.52.47(1).png",
+      link: "#"
+    },
+    {
+      name: "Neurodiverse Society",
+      img: "https://www.bcusu.com/asset/Organisation/18185/NDSoc_Logo_trasp.png",
+      link: "#"
+    },
+    {
+      name: "Parents & Carers",
+      img: "https://www.bcusu.com/asset/Organisation/9202/COMMUTER%20SOCIETY%20(1).png",
+      link: "#"
+    },
+    {
+      name: "People of Colour",
+      img: "https://www.bcusu.com/asset/Organisation/9974/POC_shield_logo_RGB_large(1).png",
+      link: "#"
+    },
+    {
+      name: "Postgraduate Society",
+      img: "https://www.bcusu.com/asset/Organisation/11869/postgrad.png",
+      link: "#"
+    },
+    {
+      name: "Sustainability Society",
+      img: "https://www.bcusu.com/asset/Organisation/10588/Sustainability%20society%20final%20logo.png",
+      link: "#"
+    },
+    {
+      name: "Womens Society",
+      img: "https://www.bcusu.com/asset/Organisation/11652/Womens_soc_logo_25_round.png",
+      link: "#"
+    }
+  ],
+    "Academic": [
+    {
+      name: "Disabled Students",
+      img: "#",
+      link: "#"
+    },
+    {
+      name: "Basketball Society",
+      img: "#",
+      link: "#"
+    },
+    {
+      name: "Disabled Students",
+      img: "#",
+      link: "#"
+    },
+    {
+      name: "Basketball Society",
+      img: "#",
+      link: "#"
+    }
+  ],
+};
