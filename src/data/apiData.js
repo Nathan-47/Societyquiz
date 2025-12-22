587e9dd9a02d552adb3c8e3c17329ace5939106d
Nathan-47 <nathan.parchment47@gmail.com>
2025-12-22 14:17:55 +0000

    Remove unused society data and API fetching logic

--- src/data/apiData.js
+++
@@ -1,5 +0,0 @@
-async function getData() {
-    const res = await fetch('https://www.bcusu.com/svc/feeds/groups/6007/?subtree=true&types=SOC');
-    const data = await res.json();
-    return data;
-}
